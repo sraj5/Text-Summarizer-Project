d0e215ec5e10d954c1a1d2019cd008e4b948a794
sraj5 <rajsaurabh2138@gmail.com>
2024-11-19 23:30:23 +0530

    data ingestion notebook and its modular coding added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Text-Summarizer-Project
+# End-to-End Text-Summarization-Project
+
+##workflows
+1. Update the config.yaml file. Its the first thing
+2. Second is updating params.yaml
+3. Update entity
+4. Update configuration manager in src/config folder
+5. Update the components (data ingestion, data validation, data transformation, model trainer)
+6. Update the pipeline
+7. Update main.py
+8. Updating app.py
---
+++ main.py
@@ -0,0 +1,16 @@
+# from textsummarizer.logging import logger
+
+# logger.info("Welcome to custom log")
+
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
