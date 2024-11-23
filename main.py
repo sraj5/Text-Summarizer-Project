eccf65c3c9fd7f4c4a719d8cfd26332b7de117b0
sraj5 <rajsaurabh2138@gmail.com>
2024-11-23 16:34:34 +0530

    model training stage(to be done in colab)

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 # logger.info("Welcome to custom log")
 
-from src.textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from src.textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-from src.textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-from src.textsummarizer.logging import logger
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textsummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textsummarizer.logging import logger
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -34,6 +35,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
